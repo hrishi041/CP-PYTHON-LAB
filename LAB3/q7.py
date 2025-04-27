86e4340ba528c41d2334f4e08b8793e4b6e39739
hrishi041 <hrishikeshkheni@gmail.com>
2025-04-27 14:48:03 +0530

    Delete LAB3 directory

--- LAB3/q7.py
+++
@@ -1,5 +0,0 @@
-year=int(input("Enter a year"))
-if(year%4==0):
-    print("leap year")
-else:
-    print("Not a leap year")
--- LAB3/q9.py
+++
@@ -1,5 +0,0 @@
-a= int(input("Enter a number"))
-if(a>0):
-    print(f"the absolute value of |a| is {a} ")
-else:
-    print(f"The absolute value of |a| is {-a}")
