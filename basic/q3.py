7d62652846b2d140c1f8661f998ac2d24d7d2196
hrishi041 <hrishikeshkheni@gmail.com>
2025-04-27 14:24:34 +0530

    Delete basic directory

--- basic/q3.py
+++
@@ -1,3 +0,0 @@
-c = int(input("Enter the temperature in celsius"))
-f = (9/5*c)+32
-print(f"The temperature in fahrenheit = {f}")
--- basic/q4.py
+++
@@ -1,4 +0,0 @@
-a =int(input("Enter th radius of the circle"))
-perimeter = 2*3.14*a
-area = 3.14*a*a
-print(f"The perimeter of the circle is {perimeter} and the area of the circle is {area}")
