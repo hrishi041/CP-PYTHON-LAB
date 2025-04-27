de51c7f17fe380f5b7dd2d1110ed7da84e3ff364
hrishi041 <hrishikeshkheni@gmail.com>
2025-04-27 15:21:44 +0530

    Add files via upload

---
+++ LAB05/q5.py
@@ -0,0 +1,15 @@
+
+def fact():
+    l=[]
+    
+    n = int(input("Enter the range"))
+    for i in range(n):
+        l.append((int(input("Enter the values of the list"))))
+        
+    for a in l:
+        fact =1
+        for j in range(1,a+1):
+            fact=fact*j
+        print(f"The factorial of numbers{a} are {fact}")
+fact()
+    
